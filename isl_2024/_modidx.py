f8701ecbb8dae36c2157c7b8fb6e0331ba2e0555
Bhargav Kowshik <bhargav.kowshik@gmail.com>
2024-09-14 18:01:34 +0530

    Scrape live match stats

--- isl_2024/_modidx.py
+++ isl_2024/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://bkowshik.github.io',
                 'git_url': 'https://github.com/bkowshik/isl-2024',
                 'lib_path': 'isl_2024'},
-  'syms': {'isl_2024.core': {'isl_2024.core.foo': ('core.html#foo', 'isl_2024/core.py')}, 'isl_2024.scrape_matches': {}}}
+  'syms': { 'isl_2024.core': {'isl_2024.core.foo': ('core.html#foo', 'isl_2024/core.py')},
+            'isl_2024.scrape_live_stats': {},
+            'isl_2024.scrape_matches': {}}}
